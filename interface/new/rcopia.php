93f4b527c4664d15adb3aedac63920b5c900b17e
Jorge <jamedina14@miners.utep.edu>
2024-02-21 22:43:46 +0000

    initial commit

---
+++ interface/new/rcopia.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Rcopia</title>
+</head>
+<body>
+<iframe src="https://web3.drfirst.com/login" width="100%" height="800" allow="camera; microphone" frameborder="0"></iframe>
+</body>
+</html>
